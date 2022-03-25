b1b3a9eb5b37d836fa0345d864a3694c57636678
woong <dndvud123@naver.com>
2022-03-26 00:15:55 +0900

    modift youtube code

--- 1_excel/Quiz.py
+++ 1_excel/Quiz.py
@@ -1,5 +1,4 @@
 from openpyxl import Workbook
-from openpyxl import load_workbook
 
 wb = Workbook()
 ws = wb.active
@@ -19,37 +18,57 @@ scores = [(1,10,8,5,14,26,12),
 for s in scores:
     ws.append(s)
 
-for row in ws.rows:
-    for cell in row:
-        if cell.column == 4 and cell.row != 1:
-            cell.value = 10
+# for row in ws.rows:
+#     for cell in row:
+#         if cell.column == 4 and cell.row != 1:
+#             cell.value = 10
+
+for idx, cell in enumerate(ws["D"]):
+    if idx == 0:
+        continue
+    cell.value = 10
 
 ws["H1"] = '총점'
-for i in range(2, 12):
-    ws[f"H{i}"] = f"=SUM(B{i}:G{i})"
-
-wb.save('scores.xlsx')
-wb.close()
-
-wb = load_workbook('scores.xlsx', data_only=True)
-ws = wb.active
-    
 ws["I1"] = "성적"
-for i in range(2, 12):
-    if ws[f"B{i}"].value >= 5:
-        if int(ws[f"H{i}"].value) >= 90:
-            ws[f"I{i}"] = 'A'
+# for i in range(2, 12):
+#     ws[f"H{i}"] = f"=SUM(B{i}:G{i})"
+# 총점과 성적 한번에 처리
+for idx, score in enumerate(scores, start=2):
+    sum_val = sum(score[1:]) - score[3] + 10
+    ws.cell(row=idx, column=8).value = f"=SUM(B{idx}:G{idx})"
+    
+    if score[1] >= 5:
+        if sum_val >= 90:
+            ws[f"I{idx}"].value = "A"
+        
+        elif sum_val >= 80:
+            ws[f"I{idx}"].value = "B"
             
-        if int(ws[f"H{i}"].value) >= 80:
-            ws[f"I{i}"] = 'B'
-            
-        if int(ws[f"H{i}"].value) >= 70:
-            ws[f"I{i}"] = 'C'
+        elif sum_val >= 70:
+            ws[f"I{idx}"].value = "C"
             
         else:
-            ws[f"I{i}"] = 'D'
+            ws[f"I{idx}"].value = "D"
     else:
-        ws[f"I{i}"] = 'F'
+        ws[f"I{idx}"].value = "F"
+
+
+# evaluate 되지 않은 상태의 데이터는 None이라고 출력
+# for i in range(2, 12):
+#     if ws[f"B{i}"].value >= 5:
+#         if int(ws[f"H{i}"].value) >= 90:
+#             ws[f"I{i}"] = 'A'
+            
+#         elif int(ws[f"H{i}"].value) >= 80:
+#             ws[f"I{i}"] = 'B'
+            
+#         elif int(ws[f"H{i}"].value) >= 70:
+#             ws[f"I{i}"] = 'C'
+            
+#         else:
+#             ws[f"I{i}"] = 'D'
+#     else:
+#         ws[f"I{i}"] = 'F'
 
         
 wb.save('scores.xlsx')
