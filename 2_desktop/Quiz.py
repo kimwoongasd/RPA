840a7cbea4111ac7e65a4eaa1c4a3f36255ee2a9
woong <dndvud123@naver.com>
2022-03-29 01:40:14 +0900

    modify youtube code

--- 2_desktop/Quiz.py
+++ 2_desktop/Quiz.py
@@ -1,15 +1,21 @@
+from curses import window
 import pyautogui
 import time
 import pyperclip
+import sys
 
+def txt_write(txt):
+    pyperclip.copy(txt)
+    pyautogui.hotkey("ctrl", "v")
+    
 # 그림판 실행
 pyautogui.hotkey("win", "r")
 pyautogui.write("mspaint")
-pyautogui.hotkey("enter")
+pyautogui.press("enter")
 
 time.sleep(1)
 
-w = pyautogui.getWindowsWithTitle("제목 없음")[0]
+w = pyautogui.getWindowsWithTitle("제목 없음 - 그림판")[0]
 
 if w.isActive == False:
     w.activate()
@@ -21,16 +27,24 @@ time.sleep(1)
 
 # 상당 텍스트 기능을 찾아서 흰 영역 아무 곳에다가 글자 입력
 txt_img = pyautogui.locateOnScreen("quiz_txt.png")
-pyautogui.click(txt_img)
+if txt_img:
+    pyautogui.click(txt_img)
 
+else:
+    sys.exit()
+    
 pyautogui.moveTo(23, 176, duration=2)
 pyautogui.leftClick()
 
+# txt_write("참 잘했어요")
 pyperclip.copy("참 잘했어요")
 pyautogui.hotkey("ctrl", "v")
 
 # 5초 대기 후 그림판 종료
-pyautogui.countdown(5)
+pyautogui.sleep(5)
+
+# 종료
+# window.close()
 pyautogui.hotkey("alt", "f4")
 time.sleep(1)
 pyautogui.press("n")
