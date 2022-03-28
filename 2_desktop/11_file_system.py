a4efbd93c5fefbb27ff773566be8f05f73c7cea4
woong <dndvud123@naver.com>
2022-03-28 22:37:28 +0900

    practice get file path

--- 2_desktop/11_file_system.py
+++ 2_desktop/11_file_system.py
@@ -1,6 +1,7 @@
 import os
 import time
 import datetime
+import fnmatch
 
 # # 현재 작업 공간
 # print(os.getcwd())
@@ -30,19 +31,47 @@ import datetime
 
 # 파일 정보 가져오기
 # 파일의 생성 날짜
-ctime = os.path.getctime("sample_insert.xlsx")
-# print(ctime)
-# # 날짜 정보를 출력
-print(datetime.datetime.fromtimestamp(ctime).strftime("%Y%m%d %H:%M:%S"))
+# ctime = os.path.getctime("sample_insert.xlsx")
+# # print(ctime)
+# # # 날짜 정보를 출력
+# print(datetime.datetime.fromtimestamp(ctime).strftime("%Y%m%d %H:%M:%S"))
 
-# 파일의 수정 날짜
-mtime = os.path.getmtime('sample_insert.xlsx')
-print(datetime.datetime.fromtimestamp(mtime).strftime("%Y%m%d %H:%M:%S"))
+# # 파일의 수정 날짜
+# mtime = os.path.getmtime('sample_insert.xlsx')
+# print(datetime.datetime.fromtimestamp(mtime).strftime("%Y%m%d %H:%M:%S"))
 
-# 파일의 마지막 접글 날자
-atime = os.path.getatime('sample_insert.xlsx')
-print(datetime.datetime.fromtimestamp(atime).strftime("%Y%m%d %H:%M:%S"))
+# # 파일의 마지막 접글 날자
+# atime = os.path.getatime('sample_insert.xlsx')
+# print(datetime.datetime.fromtimestamp(atime).strftime("%Y%m%d %H:%M:%S"))
 
-# 파일의 크기
-size = os.path.getsize('sample_insert.xlsx')
-print(size) # 바잍트 단위로 파일크기 출력
+# # 파일의 크기
+# size = os.path.getsize('sample_insert.xlsx')
+# print(size) # 바잍트 단위로 파일크기 출력
+
+# 파일 목록 가져오기
+# print(os.listdir()) # 모든 폴더, 파일 목록 가져오기
+# print(os.listdir("2_desktop")) # 주어진 폴더에서 모든 폴더 파일 목록 가져오기
+
+# 파일 목록 가져오기(하위 폴더 모두 포함)
+# 주어진 폴더 밑에 있는 모든 폴더, 파일 목록 가져오기
+# res = os.walk(".") # . : 현재 폴더
+# print(res)
+# for root, dirs, files in res:
+#     print(root, dirs, files)
+
+# 폴더 내에서 특정 파일들을 찾으려면?
+# name = "11_file_system.py"
+# res = []
+# for root, dirs, files in os.walk("."):
+#     if name in files:
+#         res.append(os.path.join(root, name))
+# print(res)
+
+# *.xlsx, *.txt 등 찾으려면?
+pattern = "*.png" # .py로 끝나는 모든 파일
+res = []
+for root, dirs, files in os.walk("."):
+    for file in files:
+        if fnmatch.fnmatch(file, pattern):
+            res.append(os.path.join(root, file))
+print(res)
