6ee34b3bfa79aa018f79dcb7917b483cba616c68
woong <dndvud123@naver.com>
2022-03-28 18:49:07 +0900

    practice image wait

--- 2_desktop/6_image_recognition.py
+++ 2_desktop/6_image_recognition.py
@@ -1,4 +1,6 @@
 import pyautogui
+import time
+import sys
 
 # # 이미지 파일과 같은 이미지를 화면에서 찾는다
 # file_menu = pyautogui.locateOnScreen("file_menu.png")
@@ -41,5 +43,53 @@ import pyautogui
 # 정확도 조정
 # 기본 값 0.999
 # 몇 퍼센트 이상 일치하면 찾는다
-run = pyautogui.locateOnScreen('run.png', confidence=0.7)
-pyautogui.moveTo(run)
+# run = pyautogui.locateOnScreen('run.png', confidence=0.7)
+# pyautogui.moveTo(run)
+
+# 자동화 대상이 바로 보여지지 않는 경우
+file_menu_notepad = pyautogui.locateOnScreen('note_menu.png')
+# if file_menu_notepad:
+#     pyautogui.moveTo(file_menu_notepad)
+# else:
+#     print("발견 실패")
+
+# 1. 계속 기다리기
+# while file_menu_notepad is None:
+#     file_menu_notepad = pyautogui.locateOnScreen('note_menu.png')
+# pyautogui.moveTo(file_menu_notepad)
+
+# 일정 시간동안 기다리기
+# timeout = 10 # 10초 대기
+# start = time.time() # 시작 시간 설정
+# file_menu_notepad = pyautogui.locateOnScreen('note_menu.png')
+# while file_menu_notepad is None:
+#     file_menu_notepad = pyautogui.locateOnScreen('note_menu.png')
+#     end = time.time() # 종료 시간 설정
+    
+#     if end - start > timeout: # 10초 초과하면 종료
+#         print("시간종료")
+#         sys.exit()
+    
+# pyautogui.moveTo(file_menu_notepad)
+
+# 함수로 만들기
+def find_target(img_file, timeout = 30):
+    start = time.time() 
+    target = None
+    while target is None:
+        target = pyautogui.locateOnScreen(img_file)
+        end = time.time()
+        
+        if end - start > timeout:
+            break
+    return target
+
+def my_click(img_file, timeout = 30):
+    target = find_target(img_file, timeout)
+    if target:
+        pyautogui.click(target)
+    
+    else:
+        print(f'{timeout}초 초과, {img_file} 파일을 찾지 못했습니다')
+        
+my_click('note_menu.png', 10)
